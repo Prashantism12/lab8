61b4fa4a72f7355a6cc44b287105af0219ae520b
Prashantism12 <pashu.s.madival@gmail.com>
2025-12-01 14:28:42 +0530

    Update test_lab8.py

--- test_lab8.py
+++ test_lab8.py
@@ -1,11 +1,6 @@
-from employee import get_employee_details
+from employee import employee_details
 
-def test_get_employee_details():
-    name = "Ravi"
-    emp_id = 101
-    department = "IT"
-    salary = 50000
-
+def test_employee_details():
     expected_output = (
         "Employee Name   : Ravi\n"
         "Employee ID     : 101\n"
@@ -13,6 +8,4 @@ def test_get_employee_details():
         "Salary          : 50000"
     )
 
-    result = get_employee_details(name, emp_id, department, salary)
-
-    assert result == expected_output
+    assert employee_details("Alice","E1001","IT",55000) == expected_output
